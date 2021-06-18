8d053e94d3a75a65485ae0f7e9e322486412ab89
RedRoach51 <RedRoach51@gmail.com>
2021-06-18 17:18:47 -0400

    Created .bat file to analyze large set of downloaded projects.

---
+++ AnalysisScript.bat
@@ -0,0 +1,50 @@
+:: This script should be placed in the same directory as 
+:: the project directories along with the .und database as well.
+
+@ECHO off
+ECHO.
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Add any directories the script should ignore here.
+set NonProjectDirectory=MockResearch UndProjects
+
+:: Set metrics that the script should record here.
+set Metrics=CountLineCode Cyclomatic AvgLineCode SumEssential
+
+
+:: Searches for project directories in local folder and updates
+:: Understand projects stored in 'UndProjects' accordingly.	
+for /D %%X in ("*") DO (
+	set validProject=true
+	for %%y in (%NonProjectDirectory%) do (
+		if %%X==%%y (
+			set validProject=false
+		)
+	)
+	if !validProject!==true (
+		echo Found Project:  %%X
+		if not exist UndProjects\%%X.und (
+			echo No previous Understand project created for %%X, initializing...
+			und create -languages java UndProjects\%%X.und 
+		)
+		und add %%X UndProjects\%%X.und
+		echo.
+	)
+)
+
+:: Perform analysis on all projects in 'UndProjects'.
+Echo Analyzing files in '\UndProjects'.
+Echo.
+
+cd UndProjects
+for /D %%X in ("*.und") do (
+	echo Analyzing Project:  %%X
+	und analyze -changed %%X
+	und settings -metricFileNameDisplayMode RelativePath %%X
+	und settings -metrics %Metrics% %%X
+	und metrics %%X
+	echo.
+) 
+
+:skip
+Echo -- AnalysisScript.bat complete --
